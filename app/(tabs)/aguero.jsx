d968629bf1116953e7f76ac5be91e34bcaa29d01
mayola <mayola@marketpsychdata.com>
2024-08-30 17:22:01 -0300

    imagenesss

---
+++ app/(tabs)/aguero.jsx
@@ -0,0 +1,22 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const aguero = () => {
+  return (
+    <ScrollView className="m-10">
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quae repellendus. Inventore reiciendis minus ab? Blanditiis, voluptate harum fuga hic maxime totam sapiente nesciunt aliquam temporibus corrupti! Laborum, laudantium aliquid.</Text>
+    </ScrollView>
+  )
+}
+
+export default aguero
---
+++ imagenes.js
@@ -0,0 +1,9 @@
+const icon = require('./assets/icon.png')
+const logo = require('./assets/logo.png')
+const menu = require('./assets/menu.png')
+
+
+export default{
+icon,
+logo
+};
