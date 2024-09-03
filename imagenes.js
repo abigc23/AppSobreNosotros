15895b3b33276a9ff6990a43b33bf0651d895c4c
mayola <mayola@marketpsychdata.com>
2024-09-03 00:09:05 -0300

    finished with aguero introduction & added some photos about him

--- imagenes.js
+++ imagenes.js
@@ -1,9 +1,19 @@
 const icon = require('./assets/icon.png')
 const logo = require('./assets/logo.png')
 const menu = require('./assets/menu.png')
-
+//aguero
+const baño = require('./assets/aguero/baño.jpeg')
+const caquita = require('./assets/aguero/caquita.jpg')
+const pelado = require('./assets/aguero/pelado.jpeg')
+const piquito = require('./assets/aguero/piquito.jpeg')
+const traficante = require('./assets/aguero/traficante.jpg')
 
 export default{
 icon,
-logo
+logo,
+baño,
+caquita,
+pelado,
+piquito,
+traficante
 };
